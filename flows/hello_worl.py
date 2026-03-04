0a08415898217b4f96b2ca552e08ce9dd7b157c9
Christoffer Boothby <chris@kribbie.com>
2026-03-04 16:08:59 +0100

    init

---
+++ flows/hello_worl.py
@@ -0,0 +1,21 @@
+from prefect import flow, task
+
+
+@task
+def add_integers(a, b):
+    return a + b
+
+
+@flow(log_prints=True)
+def buy():
+    result = add_integers(1, 2)
+    print(f"Buying securities {result}")
+
+
+if __name__ == "__main__":
+    buy.from_source(
+        "https://github.com/drowzie/dummy.git", entrypoint="flows/hello_worl:buy"
+    ).deploy(
+        name="my-code-baked-into-an-image-deployment",
+        work_pool_name="generic-bob",
+    )
