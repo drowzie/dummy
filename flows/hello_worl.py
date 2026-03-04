fd071af5bd0f1f6c487cecdcf483597aad5e014e
Christoffer Boothby <chris@kribbie.com>
2026-03-04 18:11:50 +0100

    ahah

--- flows/hello_worl.py
+++ flows/hello_worl.py
@@ -7,11 +7,17 @@ def add_integers(a, b):
     return a + b
 
 
+@task
+def subtract_integers(a, b):
+    return a - b
+
+
 @flow(log_prints=True)
 def buy():
     result = add_integers(1, 2)
     print(f"Buying securities {result}")
     time.sleep(30)
+    result = subtract_integers(1, 2)
 
 
 if __name__ == "__main__":
